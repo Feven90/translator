e1b806aff9bb31e87d7a61af5b55c7244b24aef7
Feven Mulugeta <f.mulugeta90@gmail.com>
2018-09-06 20:02:00 -0500

    added 3 objects in main.js

---
+++ Translator/main.js
@@ -0,0 +1,27 @@
+let Spanish = {
+    merry:"alegre", 
+    christmas:"Navidad",
+    and:"y",
+    happy:"contento", 
+    new:"nuevo",
+    year:"año"
+    }
+    
+let Amharic = {
+    merry:"ደስተኛ",
+    christmas:"የገና በአል", 
+    and:"እና",
+    happy:"ደስተኛ",
+    new:"አዲስ",
+    year:"አመት"
+    }
+    
+let Italian = {
+    merry:"allegro",
+    christmas:"Natale", 
+    and:"e",
+    happy:"contento",
+    new:"nuovo",
+    year:"anno"
+}
+    
