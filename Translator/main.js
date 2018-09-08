300a0569e1ef912721d2e19d915ce29af14f3daa
Feven Mulugeta <f.mulugeta90@gmail.com>
2018-09-07 23:36:33 -0500

    First feature of the translation done

--- Translator/main.js
+++ Translator/main.js
@@ -1,6 +1,6 @@
 let spanishWords = {
     merry:"alegre", 
-    christmas:"Navidad",
+    christmass:"Navidad",
     and:"y",
     happy:"contento", 
     new:"nuevo",
@@ -9,7 +9,7 @@ let spanishWords = {
     
 let amharicWords = {
     merry:"ደስተኛ",
-    christmas:"የገና በአል", 
+    christmass:"የገና በአል", 
     and:"እና",
     happy:"ደስተኛ",
     new:"አዲስ",
@@ -33,6 +33,66 @@ spanishButton.addEventListener("click", () => {
     
     for (let i = 0; i< userInput.length; i++){
         if(splitUserInput[i] === "merry"){
+            phrase = document.getElementById("translatedWord").innerHTML += ` ${spanishWords.merry} `;
+        }
+    // console.log(userInput);
+        if(splitUserInput[i] === "christmass"){
+       
+            phrase = document.getElementById("translatedWord").innerHTML += ` ${spanishWords.christmass} `;
+        }
+        if(splitUserInput[i] === "and"){
+            phrase = document.getElementById("translatedWord").innerHTML += ` ${spanishWords.and} `;
+        }
+        if(splitUserInput[i] === "happy"){
+            phrase = document.getElementById("translatedWord").innerHTML += ` ${spanishWords.happy} `;
+        }
+        if(splitUserInput[i] === "new"){
+            phrase = document.getElementById("translatedWord").innerHTML += ` ${spanishWords.new} `;
+        }
+    if(splitUserInput[i] === "year"){
+            phrase = document.getElementById("translatedWord").innerHTML += ` ${spanishWords.year} `;
+        }
+    }
+    // clearTextBox();
+})
+
+let amharicButton = document.getElementById("amharic");
+amharicButton.addEventListener("click", () => {
+    let userInput = document.getElementById("text").value;
+    let splitUserInput = userInput.split(' ');
+    // let phrase = '';
+    for (let i = 0; i< userInput.length; i++){
+        if(splitUserInput[i] === "merry"){
+            phrase = document.getElementById("translatedWord").innerHTML += ` ${amharicWords.merry} `;
+        }
+    // console.log(userInput);
+        if(splitUserInput[i] === "christmass"){
+       
+            phrase = document.getElementById("translatedWord").innerHTML += ` ${amharicWords.christmass} `;
+        }
+        if(splitUserInput[i] === "and"){
+            phrase = document.getElementById("translatedWord").innerHTML += ` ${amharicWords.and} `;
+        }
+        if(splitUserInput[i] === "happy"){
+            phrase = document.getElementById("translatedWord").innerHTML += ` ${amharicWords.happy} `;
+        }
+        if(splitUserInput[i] === "new"){
+            phrase = document.getElementById("translatedWord").innerHTML += ` ${amharicWords.new} `;
+        }
+    if(splitUserInput[i] === "year"){
+            phrase = document.getElementById("translatedWord").innerHTML += ` ${amharicWords.year} `;
+        }
+    }
+    clearTextBox();
+})
+
+let ItalicButton = document.getElementById("italic");
+ItalicButton.addEventListener("click", () => {
+    let userInput = document.getElementById("text").value;
+    let splitUserInput = userInput.split(' ');
+    // let phrase = '';
+    for (let i = 0; i< userInput.length; i++){
+        if(splitUserInput[i] === "merry"){
             phrase = document.getElementById("translatedWord").innerHTML += ` ${italianWords.merry} `;
         }
     // console.log(userInput);
@@ -53,5 +113,8 @@ spanishButton.addEventListener("click", () => {
             phrase = document.getElementById("translatedWord").innerHTML += ` ${italianWords.year} `;
         }
     }
+    clearTextBox();
 })
-
+let clearTextBox = ("click", () => {
+    document.getElementById("text").value = '';
+});
